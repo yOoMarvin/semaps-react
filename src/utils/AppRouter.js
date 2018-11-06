47588862d46f18d39ddc702618d155a4ce707bc2
yoomarvin <marvin.messenzehl@gmail.com>
2018-11-06 13:52:54 +0100

    init leaflet and basic map

--- src/utils/AppRouter.js
+++ src/utils/AppRouter.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from "../containers/App";
-import Map from "../containers/Map";
+import SimpleMap from "../containers/Map";
 
 const AppRouter = () => (
   <Router>
     <div>
       <Route path="/" exact component={App} />
-      <Route path="/map/" component={Map} />
+      <Route path="/map/" component={SimpleMap} />
     </div>
   </Router>
 );
