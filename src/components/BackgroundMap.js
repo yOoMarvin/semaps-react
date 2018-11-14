eba2950b073dfbb4ec40d3a5ce34e30943525b79
yoomarvin <marvin.messenzehl@gmail.com>
2018-11-14 14:08:12 +0100

    implement geo.json

--- src/components/BackgroundMap.js
+++ src/components/BackgroundMap.js
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
-import { Map, TileLayer } from "react-leaflet";
+import { Map, TileLayer, GeoJSON } from "react-leaflet";
+import data from "../utils/geo.json";
+
+function getAreaColor(feature) {
+  console.log(feature);
+  switch (feature.properties.name) {
+    case "Area One":
+      return "blue";
+    case "Area Two":
+      return "yellow";
+      break;
+  }
+}
 
 export default class BackgroundMap extends Component {
+  getStyle(feature, layer) {
+    return {
+      fillColor: getAreaColor(feature),
+      opacity: 0.65,
+      color: "white"
+    };
+  }
   render() {
     return (
       <Map id="map" center={this.props.center} zoom={14} zoomControl={false}>
         <TileLayer
-          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <GeoJSON data={data} style={this.getStyle} />
       </Map>
     );
   }
